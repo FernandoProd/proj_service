00a09a44c501823e66ddb0f23b60353e7b06b44f
Dmitry <katorin.dima@yandex.ru>
2025-05-11 21:26:13 +0300

    po4ti done

--- proj_service/schedule/urls.py
+++ proj_service/schedule/urls.py
@@ -1,8 +1,8 @@
-# schedule/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Пример простого URL-обработчика
     path('', views.schedule_home, name='schedule_home'),
+    path('refresh/', views.refresh_schedules, name='refresh_schedules'),
+    path('mark/<int:pk>/<str:status>/', views.mark_schedule_status, name='mark_schedule_status'),
 ]
